5b9fbcd4f1b4b807420b9bcbfcbecde2f7165303
Richard Booth <richard.booth@cpoms.co.uk>
2018-09-25 10:26:37 +0100

    Refactor crypto stuff

--- src/components/Crypto.vue
+++ src/components/Crypto.vue
@@ -1,30 +1,16 @@
 <template>
   <div class="container-fluid mt-4">
     <b-row>
-      <b-col>
-        <b-card class="text-center" title="BTC">
-          <b-card-text>
-            <font-awesome-icon icon="pound-sign"></font-awesome-icon>
-            {{ btc.data.amount }}
-          </b-card-text>
-        </b-card>
-      </b-col>
-      <b-col>
-        <b-card class="text-center" title="LTC">
-          <b-card-text>
-            <font-awesome-icon icon="pound-sign"></font-awesome-icon>
-            {{ ltc.data.amount }}
-          </b-card-text>
-        </b-card>
-      </b-col>
-      <b-col>
-        <b-card class="text-center" title="ETH">
-          <b-card-text>
-            <font-awesome-icon icon="pound-sign"></font-awesome-icon>
-            {{ eth.data.amount }}
-          </b-card-text>
-        </b-card>
-      </b-col>
+      <template v-for="c in crypto">
+        <b-col v-for="(value, key) in c">
+          <b-card class="text-center" :title="key">
+            <b-card-text>
+              <font-awesome-icon icon="pound-sign"></font-awesome-icon>
+                {{ value }}
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </template>
     </b-row>
   </div>
 </template>
@@ -32,23 +18,20 @@
 <script>
 import axios from 'axios'
 export default {
-  data () {
+  data: function () {
     return {
-      btc: null,
-      ltc: null,
-      eth: null
+      crypto: [],
+      currencies: ['BTC', 'LTC', 'ETH']
     }
   },
   mounted () {
-    axios
-      .get('https://api.coinbase.com/v2/prices/BTC-GBP/spot')
-      .then(response => (this.btc = response.data))
-    axios
-      .get('https://api.coinbase.com/v2/prices/LTC-GBP/spot')
-      .then(response => (this.ltc = response.data))
-    axios
-      .get('https://api.coinbase.com/v2/prices/ETH-GBP/spot')
-      .then(response => (this.eth = response.data))
+    for (let i = 0; i < this.currencies.length; i++) {
+      axios
+        .get(`https://api.coinbase.com/v2/prices/${this.currencies[i]}-GBP/spot`)
+        .then(response => {
+          this.crypto.push({ [this.currencies[i]]: response.data.data.amount })
+        })
+    }
   }
 }
 </script>
